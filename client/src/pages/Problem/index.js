0b8f7d5820cb9b4b5eaa46288dfc12c3a8428aa8
Peter Mirmotahari <peter.mirmotahari@gmail.com>
2020-02-04 23:04:04 -0300

    test

--- client/src/pages/Problem/index.js
+++ client/src/pages/Problem/index.js
@@ -21,6 +21,7 @@ const getProblemIdFromURL = pathname => {
   //pathname.match(/(?<=\/problem\/\s*).*?(?=\s*\/)/gs);
   if (pathname) {
     const problemIdStart = pathname.slice(9, pathname.length);
+    alert(problemIdStart);
     let problemID = "";
     for (let index in problemIdStart) {
       if (problemIdStart[index] === "/") break;
