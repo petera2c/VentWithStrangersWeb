5cfa2e94dc330ae87386d48acb50248ad66a733e
Peter Mirmotahari <peter.mirmotahari@gmail.com>
2020-03-03 13:41:16 -0300

    Bug fix

--- config/ProductionKeys.js
+++ config/ProductionKeys.js
@@ -2,6 +2,11 @@ module.exports = {
   amazonAccessKeyID: process.env.AMAZON_ACCESS_KEY_ID,
   amazonSecretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY,
   amazonBucket: process.env.AMAZON_BUCKET,
-  mongoDevelopentURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+
+  cookieKey: process.env.COOKIE_KEY,
+
+  email: process.env.EMAIL,
+  emailPassword: process.env.EMAIL_PASSWORD,
+
+  mongoDevelopentURI: process.env.MONGO_URI
 };
