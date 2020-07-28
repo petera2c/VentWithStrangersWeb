a132db9a203b54bec621b810b6c13b7176ab8e46
Peter Mirmotahari <peter.mirmotahari@gmail.com>
2020-07-28 14:37:42 -0700

    Web notifications bug fix

--- client/src/pages/util.js
+++ client/src/pages/util.js
@@ -75,13 +75,16 @@ export const initReceiveNotifications = (
 
     if (Notification.permission !== "granted") Notification.requestPermission();
     else {
-      var notification = new Notification("Notification title", {
-        icon: "http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png",
-        body: "Hey there! You've been notified!",
-      });
-      notification.onclick = function () {
-        window.open("http://stackoverflow.com/a/13328397/1269037");
-      };
+      const notificationObj = newNotifications[0];
+      if (notificationObj) {
+        var notification = new Notification(notificationObj.title, {
+          icon: "https://www.ventwithstrangers.com/favicon.ico",
+          body: notificationObj.body,
+        });
+        notification.onclick = () => {
+          window.open(notificationObj.link);
+        };
+      }
     }
 
     updateNotifications(newNotifications);
