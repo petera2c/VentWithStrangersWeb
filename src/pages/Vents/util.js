d617ed5499de3e33acc6de871ad2590224d10846
peter <peterm@ghostit.co>
2021-06-12 10:27:21 -0500

    Bug fix

--- src/pages/Vents/util.js
+++ src/pages/Vents/util.js
@@ -13,7 +13,6 @@ export const getVents = async (pathname, setCanLoadMore, setVents, vents) => {
     snapshot = await db
       .collection("/vents/")
       .where("server_timestamp", ">", startDate.valueOf())
-      .orderBy("like_counter")
       .limit(10)
       .get();
   } else
