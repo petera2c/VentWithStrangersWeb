bec47116090f5e966f2d1e232baa31d3a484afe0
peter <peterm@ghostit.co>
2021-06-15 01:24:31 -0500

    Auto liking vents

--- functions/helpers/vent.js
+++ functions/helpers/vent.js
@@ -28,8 +28,14 @@ const decreaseTrendingScore = async () => {
   }
 };
 
-const newVentListener = (doc, context) => {
+const newVentListener = async (doc, context) => {
   const vent = { id: doc.id, ...doc.data() };
+
+  await admin
+    .firestore()
+    .collection("vent_likes")
+    .doc(vent.id + "|||" + vent.userID)
+    .set({ liked: true, ventID: vent.id });
 
   return createNotification(
     createVentLink(vent),
