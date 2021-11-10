4ea99bd328eee7333654a6e5db60a129b90aee9e
peter <peterm@ghostit.co>
2021-11-10 11:01:30 -0600

    Ad bug fix attempt

--- src/pages/Vents/index.js
+++ src/pages/Vents/index.js
@@ -33,13 +33,12 @@ function Vents() {
   useEffect(() => {
     getVents(pathname, setCanLoadMore, setVents, null);
     const script = document.createElement("script");
-    script.type = "text/javascript";
 
     script.src =
       "//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b3819342-032e-406f-914b-5e96dbea3d89";
     script.async = true;
 
-    document.body.appendChild(script);
+    document.head.appendChild(script);
   }, [location]);
 
   return (
