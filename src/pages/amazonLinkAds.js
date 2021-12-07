3d2bf03e022860e5ead8ac3f74aa8f0ac0c8c77b
peter <peterm@ghostit.co>
2021-12-07 12:26:46 -0600

    more than 1 amazon ad

--- src/pages/amazonLinkAds.js
+++ src/pages/amazonLinkAds.js
@@ -2,144 +2,36 @@ import React from "react";
 import Container from "../components/containers/Container";
 
 export const amazonAdListContainer = () => {
-  const wearableBlanket = (
+  const items = [
     <iframe
-      style="width:120px;height:240px;"
-      marginwidth="0"
-      marginheight="0"
+      style={{ width: "120px", height: "240px" }}
+      marginWidth="0"
+      marginHeight="0"
       scrolling="no"
-      frameborder="0"
+      frameBorder="0"
       src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B07DKTDBWC&asins=B07DKTDBWC&linkId=9eecb46bc191d5cbc2821558e191c83a&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
+    ></iframe>,
+    <iframe
+      style={{ width: "120px", height: "240px" }}
+      marginWidth="0"
+      marginHeight="0"
+      scrolling="no"
+      frameBorder="0"
+      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B08XT9TP8X&asins=B08XT9TP8X&linkId=c2e9132ea19129bd1fa1ae0f663dd9bc&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
+    ></iframe>,
+    <iframe
+      style={{ width: "120px", height: "240px" }}
+      marginWidth="0"
+      marginHeight="0"
+      scrolling="no"
+      frameBorder="0"
+      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B08GD3WZN8&asins=B08GD3WZN8&linkId=79f5832b09cacf7dfa1c589250a9e1c7&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
     ></iframe>
-  );
+  ];
 
-  const uggSlippers = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B0009F2HDE&asins=B0009F2HDE&linkId=9472b7b90d51604344968e3a5b94d16a&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-  const bhudaBoard = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B0010TEFFQ&asins=B0010TEFFQ&linkId=fc3cd10a79cb56d16831060c70f1993a&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-  const jewelryOrganizer = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B086QWHDMV&asins=B086QWHDMV&linkId=f0fddcc3e844065feb2e36ddb0f77629&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-  const backpackPurse = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B086PHC81T&asins=B086PHC81T&linkId=cd3515974f50689f7675e4d2a78a5083&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-  const mirror = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B07VK6CR3N&asins=B07VK6CR3N&linkId=11764b87dc858f311d98aa39818d2d56&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-  const echoDot = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B07FZ8S74R&asins=B07FZ8S74R&linkId=8e9c1d8168cc24060fbb8652d03c3652&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-
-  const toque = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B002G9UDYG&asins=B002G9UDYG&linkId=f0f22ba022967812f7eba4b7fa66147f&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-
-  const masks = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B09B6V9Q8J&asins=B09B6V9Q8J&linkId=44984dc6b395160f260a1ba92a6c9b6c&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066C0&bg_color=FFFFFF"
-    />
-  );
-
-  const iphoneCase = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B09L57S7D3&asins=B09L57S7D3&linkId=d8ccaa61a7d075c18c61c63a04efe0a6&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-
-  const glassScreen = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B07H2V5YLH&asins=B07H2V5YLH&linkId=86b87acdb2d811d95ce90bca6d426d5a&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-
-  const virtualReality = (
-    <iframe
-      style={{ width: "120px", height: "240px" }}
-      marginWidth="0"
-      marginHeight="0"
-      scrolling="no"
-      frameBorder="0"
-      src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ventwithstran-20&marketplace=amazon&amp;region=US&placement=B099VMT8VZ&asins=B099VMT8VZ&linkId=acee9ba10f27a6095d3d307d24f600e6&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff"
-    />
-  );
-  /*  glassScreen,
-  masks,
-  toque,
-  echoDot,
-  mirror,
-  backpackPurse,
-  jewelryOrganizer,
-  uggSlippers,
-  virtualReality, bhudaBoard, iphoneCase,*/
   return (
     <Container className="column gap16 pa16">
-      {shuffle([wearableBlanket]).map((item, index) => (
+      {shuffle(items).map((item, index) => (
         <div key={index}>{item}</div>
       ))}
     </Container>
