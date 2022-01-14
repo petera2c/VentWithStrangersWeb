78b619f071dd41a417e736d7b1e663c14b1e8706
peter <peterm@ghostit.co>
2022-01-14 09:35:31 -0600

    Adding local keys

--- functions/config/productionKeys.js
+++ functions/config/productionKeys.js
@@ -2,6 +2,8 @@ module.exports = {
   amazonAccessKeyID: "AKIAIT66XUOZQOGRDBLQ",
   amazonSecretAccessKey: "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",
   amazonBucket: "ventwithstrangers",
-  stripePlanID: "price_1KHfJRGjCcf2qEJgNsPxQgl2",
-  stripeSecretKey: "sk_live_cMaM3AZPpZ389xzAhdo0nFQA",
+  stripePlanID: "price_1KHiCoGjCcf2qEJgrzpdkNrg",
+  stripeSecretKey: "sk_test_R9JMsggOUDZYf3mLFbZNLLgV",
 };
+//price_1KHfJRGjCcf2qEJgNsPxQgl2
+//sk_live_cMaM3AZPpZ389xzAhdo0nFQA
