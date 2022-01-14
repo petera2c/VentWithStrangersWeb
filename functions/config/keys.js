fa7c1f4689bde310a25c03e6e48e445943a2722e
peter <peterm@ghostit.co>
2022-01-14 09:50:28 -0600

    Bug fix

--- functions/config/keys.js
+++ functions/config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./ProductionKeys");
+  module.exports = require("./productionKeys");
 } else {
   module.exports = require("./localKeys");
 }
--- functions/config/productionKeys.js
+++ functions/config/productionKeys.js
@@ -1,5 +1,6 @@
 //price_1KHfJRGjCcf2qEJgNsPxQgl2
 //sk_live_cMaM3AZPpZ389xzAhdo0nFQA
+
 module.exports = {
   amazonAccessKeyID: "AKIAIT66XUOZQOGRDBLQ",
   amazonSecretAccessKey: "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",
