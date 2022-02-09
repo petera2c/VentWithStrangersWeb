466b1209a984867aaab61c8dc788569b0996c434
peter <peterm@ghostit.co>
2022-02-08 19:03:24 -0600

    Password reset bug fix

--- src/components/modals/ForgotPassword/util.js
+++ src/components/modals/ForgotPassword/util.js
@@ -1,10 +1,9 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 import { message } from "antd";
 
 export const sendPasswordReset = ({ email }) => {
-  getAuth()
-    .sendPasswordResetEmail(email)
+  sendPasswordResetEmail(getAuth(), email)
     .then(() => {
       // Email sent.
       message.success("Email password reset link sent!");
